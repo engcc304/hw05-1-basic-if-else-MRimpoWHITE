afbc9c35354cb9005b6f05783ed29dc8cce7fbc3
MRimpoWHITE <phichitphu2015@gmail.com>
2023-08-09 23:52:35 +0700

    GaiTod

--- Homework/HW05/hw05-1-if-else.cpp
+++ Homework/HW05/hw05-1-if-else.cpp
@@ -1,6 +1,6 @@
 /*
     รับตัวอักขระมาทั้งหมด 4 ค่า แล้วนำมาแสดงผลย้อนกลับ เช่น กรอกค่า L M X Y ต้องแสดงเป็น Y X M L เป็นต้น
-    
+
     Test case:
         L M Y K
     Output:
@@ -11,3 +11,19 @@
     Output:
         Result: D C B A
 */
+
+#include<stdio.h>
+
+int main() {
+    char ip[4];
+
+    printf("Input: ");
+    
+    for (int i = 0; i < 4; i++) {
+        scanf(" %c", &ip[i]);
+    }
+
+    printf("Result: %c %c %c %c", ip[3], ip[2], ip[1], ip[0]);
+
+    return 0;
+}
